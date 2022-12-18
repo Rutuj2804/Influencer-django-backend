85ad1850f679d0529dffcd9890ec39ff4a54fb14
Rutuj2804 <bokaderutuj36@gmail.com>
2022-12-18 11:39:51 +0530

    changes for vercel

--- backend/settings.py
+++ backend/settings.py
@@ -146,7 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 
 # Default primary key field type
